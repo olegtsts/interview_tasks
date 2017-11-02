913f6fba9a48f0bb7e05370b2ca7f5e35da2560e
Oleg Tsarkov <olegtsts@gmail.com>
2017-11-02 03:57:24 +0300

    Adding example of external class usage

---
+++ ExternClass.java
@@ -0,0 +1,5 @@
+public class ExternClass {
+	public static void DoAction() {
+		System.out.println("output something");
+	}
+}
---
+++ ExternClassTester.java
@@ -0,0 +1,5 @@
+public class ExternClassTester {
+	public static void main(String[] args) {
+		ExternClass.DoAction();
+	}
+}

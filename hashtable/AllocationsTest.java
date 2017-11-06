da325db0cf45906107583c4ca45a953b535c8bd9
Oleg Tsarkov <olegtsts@gmail.com>
2017-11-06 03:34:58 +0300

    Adding example of range allocation on java

---
+++ hashtable/AllocationsTest.java
@@ -0,0 +1,8 @@
+public class AllocationsTest {
+	public static void main(String[] args) {
+		Integer[] array = new Integer[3];
+		array[0] = 1;
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+	}
+};
